20f3ba711c7499f77322e55c62e836c6a9296391
bulkobubulko <bulkobubulko@gmail.com>
2023-11-08 21:23:05 +0200

    upd runner

--- runner.py
+++ runner.py
@@ -14,15 +14,32 @@ from source.lab1 import main as lab1
 from source.lab2 import main as lab2
 from source.lab3 import main as lab3
 from source.lab4 import main as lab4
+from source.lab5 import main as lab5
+from source.lab6 import main as lab6
 
 def main():
-    """
-    This function is the entry point of the program.
-    """
-    print(f"Lab1: {lab1.main()}")
-    print(f"Lab2: {lab2.main()}")
-    print(f"Lab3: {lab3.main()}")
-    print(f"Lab4: {lab4.main()}")
+    print('Choose lab:')
+    print('1. Lab1')
+    print('2. Lab2')
+    print('3. Lab3')
+    print('4. Lab4')
+    print('5. Lab5')
+    print('6. Lab6')
+    
+    user_input = input('Enter option number: ')
+    
+    if user_input == '1':
+        print(f"Lab1: {lab1.main()}")
+    if user_input == '2':
+        print(f"Lab2: {lab2.main()}")
+    if user_input == '3':
+        print(f"Lab3: {lab3.main()}")
+    if user_input == '4':
+        print(f"Lab4: {lab4.main()}")
+    if user_input == '5':
+        print(f"Lab5: {lab5.main()}")
+    if user_input == '6':
+        print(f"Lab6: {lab6.main()}")
      
 if __name__ == "__main__":
     main()
